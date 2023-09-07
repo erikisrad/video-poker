99edf0fe5b5cb5117314fc321450318a65588f7e
erikisrad <erik.rad@gmail.com>
2023-09-06 19:42:25 -0700

    implemented bonus game functionality -erik

--- VideoPoker.py
+++ VideoPoker.py
@@ -66,7 +66,7 @@ PAYOUT = { #hand name + payout multiplier
 
 
 def get_card_pic(card_name, shrink=4):
-    img = Image.open(f'./resources/cards/{card_name}.png')
+    img = Image.open(f'./resources/cards_squared/{card_name}.png')
     imgW, imgH = img.size
     new_img = img.resize((int(imgW / shrink), int(imgH / shrink)), Image.LANCZOS)
     return ImageTk.PhotoImage(new_img)
@@ -89,33 +89,45 @@ class GUI:
         col_width = 5
 
         self.cardback = get_card_pic('default2')
-        self.cardback_small = get_card_pic('default2', 12)
+        self.cardback_small = get_card_pic('default2', 16)
+        self.default_bg = self.root.cget('bg')
 
         self.main_game_frame = tk.Frame(self.root)
-        self.main_game_frame.grid(row=1, column=0, columnspan=5)
+        self.main_game_frame.grid(row=1, column=0, columnspan=5, padx=5, pady=5, sticky=tk.NSEW)
+        self.main_game_frame.grid_rowconfigure(0, weight=1)
         self.bonus_game_frame = tk.Frame(self.root)
-        self.bonus_game_frame.grid(row=0, column=0, columnspan=5)
+        self.bonus_game_frame.grid(row=0, column=0, columnspan=5, padx=5, pady=5, sticky=tk.NSEW)
+        self.bonus_game_frame.grid_rowconfigure(0, weight=1)
         for i in range(col_width):
             self.root.grid_columnconfigure(i, weight=1)
             self.main_game_frame.grid_columnconfigure(i, weight=1)
-            self.cardButtons.append(tk.Button(self.main_game_frame, image=self.cardback, bd=0))
-            self.cardButtons[i].grid(row=1, column=i, sticky=tk.NSEW, padx=5, pady=5)
+            self.cardButtons.append(tk.Button(self.main_game_frame, image=self.cardback, bd=0,
+                                              bg='black', highlightthickness=2))
+            self.cardButtons[i].grid(row=0, column=i, sticky=tk.NSEW, padx=5, pady=5)
         self.root.grid_rowconfigure(0, weight=1)
-        self.root.grid_rowconfigure(1, weight=0)
+        self.root.grid_rowconfigure(1, weight=1)
         self.root.grid_rowconfigure(2, weight=1)
 
-        d = {'frames': [], 'button_lists': []}
-        self.bonus_dataframe = pd.DataFrame(data=d)
-
-
+        bonus_list = []
         for y in range(bonus_height):
+            self.bonus_game_frame.grid_rowconfigure(y, weight=1)
             for x in range(bonus_width):
-                self.bonus_frame_array[x, y] = tk.Frame(self.bonus_game_frame)
-                self.bonus_frame_array[x, y].grid(row=x, column=y, columnspan=5)
+                self.bonus_game_frame.grid_columnconfigure(x, weight=1)
+                frame = tk.Frame(self.bonus_game_frame, highlightbackground="black", highlightthickness=2)
+                frame.grid(row=x, column=y, sticky=tk.NSEW, padx=15, pady=5)
+                frame.grid_rowconfigure(0, weight=1)
+                label = tk.Label(frame, text=' ', bd=0)
+                label.grid(row=2, column=0, columnspan=5, sticky=tk.NSEW)
+                button_list = []
                 for i in range(col_width):
-                    self.bonus_frame_array[x, y].grid_columnconfigure(i, weight=1)
-                    self.bonus_button_array[x, y].append(tk.Button(self.main_game_frame, image=self.cardback, bd=0))
-                    self.bonus_button_array[x, y][i].grid(row=0, column=i, sticky=tk.NSEW, padx=5, pady=5)
+                    frame.grid_columnconfigure(i, weight=1)
+                    button_list.append(tk.Button(frame, image=self.cardback_small, bd=0))
+                    button_list[i].grid(row=0, column=i, sticky=tk.NSEW)
+                bonus_list.append({'frame': frame, 'button_list': button_list, 'label': label})
+        self.bonus_dataframe = pd.DataFrame(bonus_list)
+
+        self.result_label = tk.Label(self.main_game_frame, text=' ', bd=0, font=tk.font.Font(size=15))
+        self.result_label.grid(row=2, column=0, columnspan=5, sticky=tk.NSEW)
 
         self.wagerFrame = tk.Frame(self.root)
         self.wagerFrame.grid(row=2, column=0, columnspan=2)
@@ -126,7 +138,7 @@ class GUI:
         self.wagerValue = tk.Label(self.wagerFrame, text='?', font=('Segoe UI', 15), anchor=tk.NW)
         self.wagerValue.pack(side="left", anchor=tk.NW)
 
-        self.playButton = tk.Button(self.root, text='?', font=('Segoe UI', 15, 'bold'), borderwidth=3, padx=0, pady=0)
+        self.playButton = tk.Button(self.root, text='?', width=15, font=('Segoe UI', 15, 'bold'), borderwidth=3, padx=0, pady=0)
         self.playButton.grid(row=2, column=2, sticky=tk.E+tk.W, pady=5)
 
         self.creditsFrame = tk.Frame(self.root)
@@ -137,6 +149,9 @@ class GUI:
 
         self.creditsValue = tk.Label(self.creditsFrame, text='?', font=('Segoe UI', 15), anchor=tk.NE)
         self.creditsValue.pack(side="left", anchor=tk.NE)
+
+        self.winningsValue = tk.Label(self.creditsFrame, text=' ', font=('Segoe UI', 15), anchor=tk.NE)
+        self.winningsValue.pack(side="left", anchor=tk.NE)
 
         game = Game(self)
 
@@ -150,12 +165,12 @@ class Game:
         self.hand = None
         self.card_images = None
         self.deck = None
-        self.wager = 100
-        self.credits = 2000
+        self.wager = 25
+        self.credits = 5000
         for suit, rank in set(itertools.product(SUITS, RANKS)):
             self.masterDeck.add(Card(suit, rank))
 
-        self.gui.wagerValue.config(text=f"${self.wager}")
+        self.gui.wagerValue.config(text=f"${self.wager} x10")
         self.gui.creditsValue.config(text=f"${self.credits}")
         self.gui.playButton.config(text="Play", command=self.start_round)
 
@@ -164,118 +179,110 @@ class Game:
         self.gui.creditsValue.config(text=f"${self.credits}")
 
     def start_round(self):
-        self.update_credits(-abs(self.wager))
+        self.update_credits(-abs(self.wager*10))
         self.deck = list(deepcopy(self.masterDeck))
         random.shuffle(self.deck)
         self.hand = []
         for i in range(5):
             self.hand.append(self.deck.pop())
             self.hand[i].gen_pic()
-            self.gui.cardButtons[i].configure(image=self.hand[i].pic, state='normal', command=lambda i=i: self.muck_card(i))
+            self.gui.cardButtons[i].configure(image=self.hand[i].pic, state='normal', command=lambda i=i: self.hold_card(i))
             self.gui.cardButtons[i].image = self.hand[i].pic
             self.gui.root.update()
             time.sleep(.2)
         self.gui.playButton.config(text="Continue", command=self.continue_round)
 
     def continue_round(self):
+        round_winnings = 0
         for i in range(5):
-            if self.hand[i].mucked:
+            self.gui.cardButtons[i].configure(bg='black')
+            if not self.hand[i].held:
+                self.gui.cardButtons[i].configure(image=self.gui.cardback)
+                self.gui.cardButtons[i].image = self.hand[i].pic
+                self.gui.root.update()
+        for index, row in self.gui.bonus_dataframe.iterrows():
+            bonus_hand = []
+            bonus_deck = list(deepcopy(self.deck))
+            random.shuffle(bonus_deck)
+            for i in range(5):
+                if self.hand[i].held:
+                    bonus_hand.append(self.hand[i])
+                else:
+                    bonus_hand.append(bonus_deck.pop())
+                    bonus_hand[i].gen_pic()
+                    row['button_list'][i].configure(image=bonus_hand[i].pic_small)
+                    row['button_list'][i].image = bonus_hand[i].pic_small
+                    self.gui.root.update()
+                    time.sleep(.2)
+            result = evaluate_hand(bonus_hand)
+            winnings = self.wager * PAYOUT[result]
+            if winnings >= self.wager:
+                row['frame'].configure(highlightbackground='green')
+            else:
+                row['frame'].configure(highlightbackground='red')
+            row['label'].configure(text=result)
+
+            round_winnings += winnings
+            print(f"Result: {result}\n"
+                  f"Winnings: {winnings}\n")
+            self.update_credits(winnings)
+
+        for i in range(5):
+            if not self.hand[i].held:
                 self.hand[i] = self.deck.pop()
                 self.hand[i].gen_pic()
+                self.gui.cardButtons[i].configure(image=self.hand[i].pic)
+                self.gui.cardButtons[i].image = self.hand[i].pic
                 self.gui.root.update()
                 time.sleep(.2)
-            self.unmuck_card(i)
-            self.gui.cardButtons[i].config(command=0)
-        result = self.evaluate_hand()
+        result = evaluate_hand(self.hand)
         winnings = self.wager * PAYOUT[result]
+
+        for i in range(5):
+            if winnings >= self.wager:
+                self.gui.cardButtons[i].configure(bg='green')
+            else:
+                self.gui.cardButtons[i].configure(bg='red')
+
+        self.gui.result_label.configure(text=result)
+
+        round_winnings += winnings
         print(f"Result: {result}\n"
               f"Winnings: {winnings}\n")
         self.update_credits(winnings)
+        print(f"round winnings: {round_winnings}")
+        self.gui.winningsValue.config(text=f"(+${round_winnings})")
         self.gui.playButton.config(text="End", command=self.end_round)
 
     def end_round(self):
+        for index, row in self.gui.bonus_dataframe.iterrows():
+            for i in range(5):
+                row['button_list'][i].config(image=self.gui.cardback_small)
+                row['button_list'][i].image = self.gui.cardback_small
+                row['frame'].configure(highlightbackground='black')
+                row['label'].configure(text=' ')
         for i in range(5):
-            self.gui.cardButtons[i].config(image=self.gui.cardback)
+            self.gui.cardButtons[i].config(image=self.gui.cardback, bg='black')
+            self.gui.cardButtons[i].image = self.gui.cardback_small
+
+        self.gui.winningsValue.config(text=' ')
+        self.gui.result_label.config(text=' ')
         self.gui.playButton.config(text="Play", command=self.start_round)
+        self.gui.root.update()
 
-    def muck_card(self, cardNum):
-        self.gui.cardButtons[cardNum].configure(image=self.gui.cardback, command=lambda i=cardNum: self.unmuck_card(i))
-        self.gui.cardButtons[cardNum].image = self.gui.cardback
-        self.hand[cardNum].mucked = True
+    def hold_card(self, cardNum):
+        self.gui.cardButtons[cardNum].configure(bg='green', command=lambda i=cardNum: self.release_card(i))
+        for x in self.gui.bonus_dataframe['button_list']:
+            x[cardNum].configure(image=self.hand[cardNum].pic_small)
+            x[cardNum].image = self.hand[cardNum].pic_small
+        self.hand[cardNum].held = True
 
-    def unmuck_card(self, cardNum):
-        self.gui.cardButtons[cardNum].configure(image=self.hand[cardNum].pic, command=lambda i=cardNum: self.muck_card(i))
-        self.gui.cardButtons[cardNum].image = self.hand[cardNum].pic
-        self.hand[cardNum].mucked = False
-
-    def evaluate_hand(self):
-        def checkFlush(cardList):
-            flush = True
-            for card in cardList[1:]:
-                if card.suit != cardList[0].suit:
-                    flush = False
-                    break
-            return flush
-
-        def checkStraight(cardList):  # if cards are consecutive - ace can be high or low so check for A,2,3,4,5 as well
-            cardList = [x.rank for x in cardList]
-            return (len(set(cardList)) == len(cardList) and max(cardList) - min(cardList) == len(cardList) - 1) or \
-                   (sorted(cardList) == [2, 3, 4, 5, 14])
-
-        def checkPairing(cardList):
-            values = ([x.rank for x in cardList])
-            value_counts = defaultdict(int)
-            for v in values:
-                value_counts[v] += 1
-            result = []
-            for k, v in value_counts.items():
-                if v > 1:
-                    result.append([k, v])
-
-            pairs = 0
-            sets = 0
-            quads = 0
-            jackhigh = False
-            for match in result:
-                if match[1] == 2:
-                    pairs += 1
-                    if match[0] > 10:
-                        jackhigh = True
-                elif match[1] == 3:
-                    sets += 1
-                elif match[1] == 4:
-                    quads += 1
-            if quads == 1:
-                return QUADS
-            elif sets == 1 and pairs == 1:
-                return FULLHOUSE
-            elif pairs == 2:
-                return TWOPAIR
-            elif sets == 1:
-                return THREE
-            elif pairs == 1 and jackhigh:
-                return JORB
-            elif pairs == 1 and not jackhigh:
-                return LOWPAIR
-            else:
-                return NOTHING
-
-        straight_result = checkStraight(self.hand)
-        flush_result = checkFlush(self.hand)
-
-        if straight_result and flush_result:
-            if sorted([x.rank for x in self.hand]) == [10, 11, 12, 13, 14]:
-                win = ROYALFLUSH
-            else:
-                win = STRAIGHTFLUSH
-        elif straight_result:
-            win = STRAIGHT
-        elif flush_result:
-            win = FLUSH
-        else:
-            win = checkPairing(self.hand)
-
-        return win
+    def release_card(self, cardNum):
+        self.gui.cardButtons[cardNum].configure(bg='black', command=lambda i=cardNum: self.hold_card(i))
+        for x in self.gui.bonus_dataframe['button_list']:
+            x[cardNum].configure(image=self.gui.cardback_small)
+            x[cardNum].image = self.gui.cardback_small
+        self.hand[cardNum].held = False
 
 
 class Card:
@@ -284,8 +291,9 @@ class Card:
         self.prettySuit = SUITS[suit]
         self.rank = rank
         self.prettyRank = RANKS[rank]
-        self.mucked = False
+        self.held = False
         self.pic = None
+        self.pic_small = None
 
     def __str__(self):
         return f"{self.prettyRank}{self.prettySuit}"
@@ -296,6 +304,77 @@ class Card:
     def gen_pic(self):
         if self.pic is None:
             self.pic = get_card_pic(f"{self.prettyRank}{self.prettySuit}")
+            self.pic_small = get_card_pic(f"{self.prettyRank}{self.prettySuit}", 16)
+
+
+def evaluate_hand(hand_list):
+    def checkFlush(cardList):
+        flush = True
+        for card in cardList[1:]:
+            if card.suit != cardList[0].suit:
+                flush = False
+                break
+        return flush
+
+    def checkStraight(cardList):  # if cards are consecutive - ace can be high or low so check for A,2,3,4,5 as well
+        cardList = [x.rank for x in cardList]
+        return (len(set(cardList)) == len(cardList) and max(cardList) - min(cardList) == len(cardList) - 1) or \
+               (sorted(cardList) == [2, 3, 4, 5, 14])
+
+    def checkPairing(cardList):
+        values = ([x.rank for x in cardList])
+        value_counts = defaultdict(int)
+        for v in values:
+            value_counts[v] += 1
+        result = []
+        for k, v in value_counts.items():
+            if v > 1:
+                result.append([k, v])
+
+        pairs = 0
+        sets = 0
+        quads = 0
+        jackhigh = False
+        for match in result:
+            if match[1] == 2:
+                pairs += 1
+                if match[0] > 10:
+                    jackhigh = True
+            elif match[1] == 3:
+                sets += 1
+            elif match[1] == 4:
+                quads += 1
+        if quads == 1:
+            return QUADS
+        elif sets == 1 and pairs == 1:
+            return FULLHOUSE
+        elif pairs == 2:
+            return TWOPAIR
+        elif sets == 1:
+            return THREE
+        elif pairs == 1 and jackhigh:
+            return JORB
+        elif pairs == 1 and not jackhigh:
+            return LOWPAIR
+        else:
+            return NOTHING
+
+    straight_result = checkStraight(hand_list)
+    flush_result = checkFlush(hand_list)
+
+    if straight_result and flush_result:
+        if sorted([x.rank for x in hand_list]) == [10, 11, 12, 13, 14]:
+            win = ROYALFLUSH
+        else:
+            win = STRAIGHTFLUSH
+    elif straight_result:
+        win = STRAIGHT
+    elif flush_result:
+        win = FLUSH
+    else:
+        win = checkPairing(hand_list)
+
+    return win
 
 
 if __name__ == '__main__':
